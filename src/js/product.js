c437dec87d3ba78a35af81b692491084b2f15337
SofiaAS784 <ssuarez2@byupathway.edu>
2026-03-05 17:35:59 -0300

    Week 01 activity - My Solution

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,12 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
+let productsArray = getLocalStorage("so-cart") || [];
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  productsArray.push(product);
+  setLocalStorage("so-cart", productsArray);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
