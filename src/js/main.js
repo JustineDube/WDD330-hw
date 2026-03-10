4ec38f79bedc444feb883f5d5e209ea1c18a6e2c
jsbowen79 <jsbowen79@outlook.com>
2026-03-09 19:55:01 -0600

    Completed personal assignment

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
+import ProductData from "./ProductData.mjs";
+import { ProductList } from "./ProductList.mjs";
+
+
+const source = new ProductData("tents");
+const listElement = document.querySelector('.product-list');
+const productList = new ProductList(source, listElement); 
+productList.init();
+
 const nodeList = document.querySelectorAll(".products a"); 
 nodeList.forEach(element => {
     element.addEventListener('click', () => {
