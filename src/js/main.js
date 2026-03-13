eacccf5d8bdaa67db169bca53fb2cc3543d757e9
jsbowen79 <jsbowen79@outlook.com>
2026-03-13 17:03:16 -0600

    Week 2 ready to merge with team

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,3 @@ const listElement = document.querySelector(".product-list");
 const productList = new ProductList(source, listElement);
 productList.init();
 
-const nodeList = document.querySelectorAll(".products a");
-nodeList.forEach((element) => {
-  element.addEventListener("click", () => {});
-});
